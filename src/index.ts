1aadfefad82b90c114062b0abbcd68f22437209c
Chris <krasnyineko@gmail.com>
2020-09-27 23:12:06 -0500

    working check_neighbors function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-import { Cell } from "./cell";
+import { Cell, CellState } from "./cell";
+import {max, min} from 'mathjs'
 
-export function check_neighbors(board: Cell[][], x: number, y: number): number{
-    board
-    x
-    y
-    return 0
+export function check_neighbors(board: Cell[][], row: number, col: number): number{
+    if(board.length === 0) return 0
+
+    const boardSize = board.length
+    let neighborCount = 0
+
+    for(let y = max(0, col - 1); y <= min(col + 1, boardSize); y++){
+        for(let x = max(0, row - 1); x <= min(row + 1, boardSize); x++){
+            if(y === col && x === row) continue
+
+            const cell = board[x][y]
+            neighborCount += cell.state === CellState.Alive ? 1 : 0
+        }
+    }
+
+    return neighborCount
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,7 +2,7 @@ import 'mocha'
 import { expect } from 'chai'
 
 import { check_neighbors } from '../src/index'
-import { Cell } from '../src/cell';
+import { Cell, CellState } from '../src/cell';
 
 describe('Check neighbors function', () => {
     describe('basic cases', () => {
@@ -16,14 +16,24 @@ describe('Check neighbors function', () => {
             expect(check_neighbors(board, 1, 1)).to.equal(0)
         })
 
-        it('should return 1 when there are no living neighbors', () => {
+        it('should return 1 when there are is 1 living neighbors', () => {
             const board = [
                 [new Cell(), new Cell(), new Cell()],
-                [new Cell(), new Cell(), new Cell()],
+                [new Cell(), new Cell(), new Cell(CellState.Alive)],
                 [new Cell(), new Cell(), new Cell()]
             ]
 
-            expect(check_neighbors(board, 1, 1)).to.equal(0)
+            expect(check_neighbors(board, 1, 1)).to.equal(1)
+        })
+
+        it('should return 8 when there are is 8 living neighbors', () => {
+            const board = [
+                [new Cell(CellState.Alive), new Cell(CellState.Alive), new Cell(CellState.Alive)],
+                [new Cell(CellState.Alive), new Cell(), new Cell(CellState.Alive)],
+                [new Cell(CellState.Alive), new Cell(CellState.Alive), new Cell(CellState.Alive)]
+            ]
+
+            expect(check_neighbors(board, 1, 1)).to.equal(8)
         })
     })
 })
