e9004093f821d6a3efea551f748aa547a3b3cf25
Crysthian Chavez <cc4125@it-crys.klec.klecasino.com>
2020-09-28 13:51:12 -0500

    formatting

--- src/cell.ts
+++ src/cell.ts
@@ -1,27 +1,27 @@
 import { min, max } from "mathjs";
 
-export enum CellState{
+export enum CellState {
     Alive,
     Dead
 }
 
-export class Cell{
+export class Cell {
     state: CellState
-    
+
     constructor(initial_state: CellState = CellState.Dead) {
         this.state = initial_state
     }
 }
 
-export function check_neighbors(board: Cell[][], row: number, col: number): number{
-    if(board.length === 0) return 0
+export function check_neighbors(board: Cell[][], row: number, col: number): number {
+    if (board.length === 0) return 0
 
     const boardSize = board.length
     let neighborCount = 0
 
-    for(let y = max(0, col - 1); y <= min(col + 1, boardSize); y++){
-        for(let x = max(0, row - 1); x <= min(row + 1, boardSize); x++){
-            if(y === col && x === row) continue
+    for (let y = max(0, col - 1); y <= min(col + 1, boardSize); y++) {
+        for (let x = max(0, row - 1); x <= min(row + 1, boardSize); x++) {
+            if (y === col && x === row) continue
 
             const cell = board[x][y]
             neighborCount += cell.state === CellState.Alive ? 1 : 0
