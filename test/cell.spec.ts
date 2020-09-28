4ac75c9b2a8e71408b76c472b88e69cef5d9b0e5
Chris <krasnyineko@gmail.com>
2020-09-27 23:14:36 -0500

    moved check_neighbors function to cells.ts

--- test/cell.spec.ts
+++ test/cell.spec.ts
@@ -1,7 +1,7 @@
 import 'mocha'
 import {expect} from 'chai'
 
-import { Cell, CellState } from '../src/cell'
+import { Cell, CellState, check_neighbors } from '../src/cell'
 
 describe('Cell', () => {
     describe('Initialization', () => {
@@ -18,6 +18,40 @@ describe('Cell', () => {
         it('should set state to dead', () => {
             const cell = new Cell(CellState.Dead)
             expect(cell.state).to.equal(CellState.Dead)
+        })
+    })
+})
+
+describe('Check neighbors function', () => {
+    describe('basic cases', () => {
+        it('should return 0 when there are no living neighbors', () => {
+            const board = [
+                [new Cell(), new Cell(), new Cell()],
+                [new Cell(), new Cell(), new Cell()],
+                [new Cell(), new Cell(), new Cell()]
+            ]
+
+            expect(check_neighbors(board, 1, 1)).to.equal(0)
+        })
+
+        it('should return 1 when there are is 1 living neighbors', () => {
+            const board = [
+                [new Cell(), new Cell(), new Cell()],
+                [new Cell(), new Cell(), new Cell(CellState.Alive)],
+                [new Cell(), new Cell(), new Cell()]
+            ]
+
+            expect(check_neighbors(board, 1, 1)).to.equal(1)
+        })
+
+        it('should return 8 when there are is 8 living neighbors', () => {
+            const board = [
+                [new Cell(CellState.Alive), new Cell(CellState.Alive), new Cell(CellState.Alive)],
+                [new Cell(CellState.Alive), new Cell(), new Cell(CellState.Alive)],
+                [new Cell(CellState.Alive), new Cell(CellState.Alive), new Cell(CellState.Alive)]
+            ]
+
+            expect(check_neighbors(board, 1, 1)).to.equal(8)
         })
     })
 })
